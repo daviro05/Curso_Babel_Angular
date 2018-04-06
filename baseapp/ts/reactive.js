4f45a66f9cf02c66cdfccdab97486937f5caacd4
David Rodriguez <david.rmarco@babel.es>
2018-04-06 13:58:58 +0200

    Observables Ejemplo

---
+++ baseapp/ts/reactive.js
@@ -0,0 +1,22 @@
+var Rx = require('rxjs/Rx')
+//import * as Rx from 'rxjs/Rx'
+
+let aDatos = [1,2,3,4,5,6];
+
+
+const observable = Rx.Observable
+.from(aDatos)
+.map( x => x*x)
+.filter(x=> x > 15);
+
+/* const observador = Rx.Observer.create(
+    function onNext(x) {console.log('Next:' + x)},
+    function onError(err) {console.log('Err:' + err)},
+    function onCompleted() {console.log('Completed')}
+); */
+
+observable.subscribe(
+    function onNext(x) {console.log('Next:' + x)},
+    function onError(err) {console.log('Err:' + err)},
+    function onCompleted() {console.log('Completed')}
+);
